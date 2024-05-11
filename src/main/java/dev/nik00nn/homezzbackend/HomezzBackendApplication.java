fd05f4773c27e9ba57fa71aa488b7fba2deb19cd
Nik00NN <birziloiu.mihaialexandru@outlook.com>
2024-05-12 02:01:43 +0300

    Initial commit

---
+++ src/main/java/dev/nik00nn/homezzbackend/HomezzBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.nik00nn.homezzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomezzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomezzBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nik00nn/homezzbackend/HomezzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nik00nn.homezzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomezzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
